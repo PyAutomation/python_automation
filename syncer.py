259fb159e225828c30f1b0ac539ec880d828f443
gendos379 <gendos20@yandex.ru>
2016-04-15 20:45:17 +0300

    Added files via upload

--- syncer.py
+++ syncer.py
@@ -1,45 +1,56 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 import os
 import sys
 import paramiko
 
-def argument_check():
-    #will check if arguments are ok
+# def argument_check():
+#     #will check if arguments are ok
 
 def ip_check():
+    hosts = []
+    valid_hosts = []
     for item in sys.argv:
         if '@' in item:
-            ip_path = item.split('@')[1]
-            host = ip_path.split(':')[0]
-            command = os.system('ping -c 3 '+host)
-            if command == 0:
-                path_check()
-            else:
-                print('Remote host is down')
+            hosts.append(item)
+    for i in hosts:
+        host = i.split('@')[1].split(':')[0]
+        command = os.system('ping -c 1 '+host)
+        if command == 0:
+            valid_hosts.append(i)
+    if valid_hosts:
+        path_check(valid_hosts)
 
-def path_check():
+def path_check(hosts):
     for item in sys.argv:
-        if '@' in item:
-            user_port, host_path = item.split('@')
-            host, path = host_path.split(':')
-            for separator in ',.:':
-                if separator in user_port:
-                    user, port = user_port.split(separator)
-                    break
-                else:
-                    user = user_port
-                    port = ''
         if '–pass' in item:
             secret = item.split('=')[1].strip("'")
+            break
         else:
             secret = ''
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=host, username=user, password=secret, port=port)
-    stdin, stdout, stderr = client.exec_command('mkdir -p '+path)
+    counter = 1
+    for i in hosts:
+        user_port, host_path = i.split('@')
+        host, path = host_path.split(':')
+        for separator in ',.:':
+            if separator in user_port:
+                user, port = user_port.split(separator)
+                break
+            else:
+                user = user_port
+                port = ''
+        print(str(counter)+'. Username: '+user+', Port: '+port+' , ip: '+host+', Remote path: '+path)
+        counter += 1
+    if secret:
+        print('Password: '+secret)
+    # client = paramiko.SSHClient()
+    # client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # client.connect(hostname=host, username=user, password=secret, port=port)
+    # stdin, stdout, stderr = client.exec_command('mkdir -p '+path)
 
 
-
-def copy_file():
-    arguments = ' '.join(sys.argv[1:])
-    os.system('rsync '+arguments)
+#
+# def copy_file():
+#     arguments = ' '.join(sys.argv[1:])
+#     os.system('rsync '+arguments)
+ip_check()
